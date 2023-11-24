f78876c92f83ee8a70225e8ddeff7a698486a8bd
Ailton Occhi <ailton.occhi@hotmail.com>
2023-11-23 21:35:09 -0300

    Add Projetos 9 e 10

---
+++ 09 - Projeto Compartilhado/src/br/com/treinamento/calculos/Calculos.java
@@ -0,0 +1,19 @@
+package br.com.treinamento.calculos;
+
+public class Calculos {
+	
+	
+	public static Integer somarDoisNumeros(Integer numero1, Integer numero2){
+		return numero1+numero2;
+	}
+	
+	public static Double calculaCircunferencia(Integer diametro) {
+		Double pi = 3.14159265;
+		return pi*diametro;
+	}
+	
+	public static Integer caculaAreaQuadrado(Integer base, Integer altura) {
+		return base*altura;
+	}
+
+}
